1462cf0d6f7e3af5fd7ccb23d81184f4a886c819
yangyliu <yangyliu@ebay.com>
2021-12-22 21:09:49 +0800

    add basic interface knowledge

--- basic-latest/5interface.ts
+++ basic-latest/5interface.ts
@@ -1,9 +1,54 @@
 
+//interface中的 ? readonly [prop: string]: any, function 
+interface Person {
+  readonly name: string,
+  age?: number,
+  // [propName: string]: any,
+  greet(): string,
+}
 
+// 字面量传参 和 变量形式传参 区别
+const person = {
+  name: 'Amy',
+  age: 18, 
+  gender: 'female',
+  height: 170,
+  degree: 'PHD',
+  greet() {
+    return 'hello world'
+  }
+};
+const getName = (person: Person) => {
+  return person.name;
+}
+const setName = (person: Person): void => {
+  // person.name = 'cannot change';
+}
+getName(person);
+// getName({name: 'Amy', degree: 'PHD', greet: () => ''}); //在interface没有[propName: string]: any时,自面量传参不能有多出interface的属性
 
+// class 应用 interface, class中必须包含 interface 中必须的属性
+class User2 implements Person {
+  name = 'Sheldon';
+  greet:() => 'hell world';
+  degree = 'PHD';
+  height = 185;
+}
 
+// interface 继承 interface, 新的interface也包含被继承的interface
+interface Teacher extends Person {
+  teach(): string,
+}
+const teacher: Teacher = {
+  teach: () => 'Computer Science',
+  name: 'Bert',
+  greet: () => '',
+};
 
-
-
-
-
+// interface 定义 函数类型
+interface SaySth {
+  (word: string): string,
+}
+const say: SaySth = (sth) => {
+  return sth;
+};
